c14ee19e179de093d0b4a71e30e33805279673ee
juno <pawelekstanley@gmail.com>
2023-12-30 17:03:35 +0100

    final2

--- src/dovecote.rs
+++ src/dovecote.rs
@@ -30,33 +30,33 @@ impl DoveCote {
         self.total_doves += 1;
         let curr_count = self.boxes[box_num];
 
-        // if curr_count == 1 {
-        //     if self.total_doves < self.boxes.len() {
-        //         self.empty_boxes -= 1;
-        //     }
-        //     self.all_one += 1;
-        //     // self.empty_boxes_fn.set(self.total_doves, self.empty_boxes);
-        // } else if curr_count == 2 {
-        //     self.all_two += 1;
-        //     if self.first_collision == 0 {
-        //         self.first_collision = self.total_doves;
-        //     }
-        // }
+        if curr_count == 1 {
+            if self.total_doves < self.boxes.len() {
+                self.empty_boxes -= 1;
+            }
+            self.all_one += 1;
+            // self.empty_boxes_fn.set(self.total_doves, self.empty_boxes);
+        } else if curr_count == 2 {
+            self.all_two += 1;
+            if self.first_collision == 0 {
+                self.first_collision = self.total_doves;
+            }
+        }
 
         if self.total_doves <= self.boxes.len() {
             if curr_count > self.max_doves {
                 self.max_doves = curr_count;
             }
         } else {
-            return false;
+            // return false;
         }
 
-        // if self.all_one == self.boxes.len() {
-        //     self.all_one = self.total_doves;
-        // } else if self.all_two == self.boxes.len() {
-        //     self.all_two = self.total_doves;
-        //     return false;
-        // }
+        if self.all_one == self.boxes.len() {
+            self.all_one = self.total_doves;
+        } else if self.all_two == self.boxes.len() {
+            self.all_two = self.total_doves;
+            return false;
+        }
         return true;
     }
 
